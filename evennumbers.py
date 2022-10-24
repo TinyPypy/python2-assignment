a58821b9f0d5732b22fa8ebd3d65d798cdce3d5f
Munene Patience <114987649+TinyPypy@users.noreply.github.com>
2022-10-24 10:26:03 +0000

    first commit

---
+++ evennumbers.py
@@ -0,0 +1,14 @@
+
+for number in range (1, 31):
+    if (number  % 2 == 0):
+        print(f"Number: {number}")
+    
+
+number = 1
+while number<=30:
+    if number%2 == 0:
+        print(f"Numero: {number}")
+    number = number + 1
+    
+
+
---
+++ financial.py
@@ -0,0 +1,12 @@
+budget =50,000
+marketing= 25/100*50000
+expenses= 50/100*50000
+customerAquisition=25/100*50000
+
+print("Ahmed's Finacial statement")
+print(f"Marketing expenditure:{marketing}")
+print(f"Expenses expenditure: {expenses}")
+print(f"Customer Aquisition expenditure: {customerAquisition}")
+
+customer = customerAquisition/5
+print(f"With his budget, Ahmed can aquire, {customer} customers.")
